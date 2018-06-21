2c32fa4303a93d2223bad19906306d1b980fe07a
gitApp <horroona@gmail.com>
2018-06-21 15:11:26 +0500

    file updated

--- StudentManagementSystem.php
+++ StudentManagementSystem.php
@@ -1,3 +1,48 @@
+<!DOCTYPE html>
 <?php
-	print("Haroon");
+
+	function display(){
+
+		$name = $_POST['name'];
+		print ("Hello " . $name);
+	}
+
+	if(isset($_POST['submit'])){
+		display();
+	}
 ?>
+<html>
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>Admin Login</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+
+	<script src="main.js"></script>
+
+</head>
+
+<body>
+
+	<form action="" method ="post">
+
+		<input type="text" name ="name" placeholder ="User">
+		</br>
+		</br>
+		<input type="password" name ="password" placeholder ="password">
+		</br>
+		</br>
+		<input type="submit" value ="click" name ="submit">
+	
+	</form>
+
+</body>
+
+</html>
